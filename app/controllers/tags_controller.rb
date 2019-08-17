90a9f88463e08542708098abf4964f5c548deace
mahmoud <mahmoudmohamed762017@gmail.com>
2019-08-18 01:33:56 +0200

    Tagging feature completed

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,33 @@
+class TagsController < ApplicationController
+
+    def index
+        @tags = Tag.all
+    end
+
+    def show
+        @tag = Tag.find(params[:id])
+    end
+
+#    def destroy
+#        @tag = Tag.find(params[:id])
+#        @tag.destroy
+#        
+#        flash.notice = "Tag '#{@tag.name}' Destroyed"
+#
+#        redirect_to tags_path
+#    end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @taggings = Tagging.where(tag_id: @tag.id)
+
+    @taggings.each do |tagging|
+        tagging.destroy
+    end
+
+    @tag.destroy
+
+    redirect_to  tags_path
+  end
+
+end
